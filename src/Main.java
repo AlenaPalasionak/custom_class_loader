cd61c582a26fcfaae1750ec12e88f785a09ef37d
Alena <19lenka87@tut.by>
2025-03-02 10:51:50 +0300

    3. Made some corrections

--- src/Main.java
+++ src/Main.java
@@ -22,27 +22,34 @@ public class Main {
         try {
             Class<?> driverClass = myClassLoader.findClass("external_classes.Driver");
             Class<?> carClass = myClassLoader.findClass("external_classes.Car");
+
             String driverName = "Schumacher";
             int plateNumber = 333;
             String carMake = "Ferrari";
+
             Constructor<?> driverConstructor = driverClass.getConstructor(String.class);
             Object driverInstance = driverConstructor.newInstance(driverName);
             Constructor<?> carConstructor = carClass.getConstructor(driverClass, int.class, String.class);
             Object carInstance = carConstructor.newInstance(driverInstance, plateNumber, carMake);
+
             Method startEngineMethod = carClass.getMethod("startEngine");
             Method getDriverMethod = carClass.getMethod("getDriver");
             Method getPlateNumberMethod = carClass.getMethod("getPlateNumber");
             Method getMakeMethod = carClass.getMethod("getMake");
-            System.out.println("Method invocation - startEngine: " + startEngineMethod.invoke(carInstance) + "\n");
-            System.out.println("Method invocation - getDriverMethod: " + getDriverMethod.invoke(carInstance) + "\n");
-            System.out.println("Method invocation - getPlateNumberMethod: " + getPlateNumberMethod.invoke(carInstance) + "\n");
-            System.out.println("Method invocation - getMakeMethod: " + getMakeMethod.invoke(carInstance) + "\n");
+
+            System.out.println("Method invocation - startEngine:");
+            startEngineMethod.invoke(carInstance);
+
+            System.out.println("Method invocation - getDriver: " + getDriverMethod.invoke(carInstance) + "\n");
+            System.out.println("Method invocation - getPlateNumber: " + getPlateNumberMethod.invoke(carInstance) + "\n");
+            System.out.println("Method invocation - getMake: " + getMakeMethod.invoke(carInstance) + "\n");
 
             System.out.println("Car Class name: " + carClass.getName() + "\n");
             System.out.println("Car Fields: " + Arrays.toString(carClass.getDeclaredFields()) + "\n");
             System.out.println("Car Package: " + carClass.getPackage() + "\n");
             System.out.println("Car ClassLoader: " + carClass.getClassLoader().getClass() + "\n");
-            System.out.println("Car Methods: " + Arrays.toString(carClass.getMethods()) + "\n");
+            System.out.println("Car Methods including inherited from Object: \n"
+                    + Arrays.toString(carClass.getMethods()));
         } catch (ClassNotFoundException e) {
             System.err.println("Class is not found " + e.getMessage());
         } catch (Exception e) {
